34f61b75f799a38d59e93edcf3f0f4aa1fdc9b19
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2015-07-18 15:13:52 +0200

    debug

--- src/photobooth.py
+++ src/photobooth.py
@@ -69,38 +69,6 @@ def savePhoto():
     shutil.move(captureName, config['Paths']['photos'] + '/' 
                 + config['Misc']['imagePrefix'] + safeTime + '.jpg')
 
-def aspect_scale(img,(bx,by)):
-    ix,iy = img.get_size()
-    
-    if ix > iy:
-        # fit to width
-        scale_factor = bx/float(ix)
-        sy = scale_factor * iy
-        
-        if sy > by:
-            scale_factor = by/float(iy)
-            sx = scale_factor * ix
-            sy = by
-        
-        else:
-            sx = bx
-    
-    else:
-        # fit to height
-        scale_factor = by/float(iy)
-        sx = scale_factor * ix
-        
-        if sx > bx:
-            scale_factor = bx/float(ix)
-            sx = bx
-            sy = scale_factor * iy
-        
-        else:
-            sy = by
-
-    return pygame.transform.scale(img, (sx,sy))
-
-
 ## ----- Setup -----------------------------------------------------------------
 
 setupPygame()
---
+++ src/util.py
@@ -0,0 +1,32 @@
+import pygame
+
+def aspectScale(img, (bx, by)):
+    ix, iy = img.get_size()
+    
+    if ix > iy:
+        # fit to width
+        scale_factor = bx/float(ix)
+        sy = scale_factor * iy
+        
+        if sy > by:
+            scale_factor = by/float(iy)
+            sx = scale_factor * ix
+            sy = by
+        
+        else:
+            sx = bx
+    
+    else:
+        # fit to height
+        scale_factor = by/float(iy)
+        sx = scale_factor * ix
+        
+        if sx > bx:
+            scale_factor = bx/float(ix)
+            sx = bx
+            sy = scale_factor * iy
+        
+        else:
+            sy = by
+
+    return pygame.transform.scale(img, (int(sx), int(sy)))
